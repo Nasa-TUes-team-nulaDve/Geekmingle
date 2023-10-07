1c650073131d8423bdfe0cc219eb7c1aa861885d
Svetoslav47 <svetoslav2806@gmail.com>
2023-10-07 12:21:22 +0300

    navbar start and jwt util request

--- geekmingleclient/src/index.js
+++ geekmingleclient/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { UserProvider } from './Contexts/userContext';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -12,11 +13,13 @@ import Register from './pages/Register';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <UserProvider>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
+  </UserProvider>
 );
